970dc90354386f88c74d17790f4ada28e78db657
songye38 <vsongyev@hanmail.net>
2017-10-27 14:41:19 +0900

    add example3-1 source code

---
+++ Session1/src/session1_example3-1.py
@@ -0,0 +1,22 @@
+import rhinoscriptsyntax as rs
+import random
+import math
+
+z = -100  #height start from -100 to 100
+radius = 5
+theta = 0
+
+count =0
+while z <=100:
+    if count %2 ==0:
+        x = (radius+5) * math.cos(theta)
+        y = (radius+5) * math.sin(theta)
+    else:
+        x = radius * math.cos(theta)
+        y = radius * math.sin(theta)
+
+    rs.AddPoint((x,y,z))
+    radius += .003
+    theta += 1
+    z += .01
+    count+=1
